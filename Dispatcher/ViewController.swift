98029b5d3bbb837aae71d227252fd0468f2efc9f
ofekcMoveo <ofekc@moveo.co.il>
2023-04-16 15:53:53 +0300

    Initial Commit

---
+++ Dispatcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dispatcher
+//
+//  Created by Ofek Cohen on 16/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
