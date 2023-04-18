77e1bb273cd3a04622b76ff548d8e19b1c734de4
ofekcMoveo <ofekc@moveo.co.il>
2023-04-18 18:57:02 +0300

    header icons moved out from NavigationController to a UIView in HomeController

--- Dispatcher/Controllers/HomeViewController.swift
+++ Dispatcher/Controllers/HomeViewController.swift
@@ -14,6 +14,8 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+
     }
     
     
