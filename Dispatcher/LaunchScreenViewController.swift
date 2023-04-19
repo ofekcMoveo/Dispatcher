a3409b49e6724b6c84765a95e8a416d9d82336b1
ofekcMoveo <ofekc@moveo.co.il>
2023-04-19 10:59:03 +0300

    use main.storyBoard and VC to display and delay the launch screen + deleting  contradicting and unnecessary constraints

---
+++ Dispatcher/LaunchScreenViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LaunchScreenViewController.swift
+//  Dispatcher
+//
+//  Created by Ofek Cohen on 19/04/2023.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        sleep(3)
+    }
+
+}
