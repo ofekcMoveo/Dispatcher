e290aa476ebe41bf1e8f87d3c3ff4eb5e55810cb
ofekcMoveo <ofekc@moveo.co.il>
2023-05-18 00:09:32 +0300

    no message

---
+++ Dispatcher/Constants/TextCostants.swift
@@ -0,0 +1,16 @@
+//
+//  TextCostants.swift
+//  Dispatcher
+//
+//  Created by Ofek Cohen on 14/05/2023.
+//
+
+import Foundation
+
+struct TextCostants {
+
+    static let emailTextFieldPlaceholder = "Your email"
+    static let passwordTextFieldPlaceholder = "Password"
+    static let reEnterPasswordTextFieldPlaceholder = "ReEnter Password"
+
+}
