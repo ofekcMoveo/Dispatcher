9aaf597f687c14460715029b09c0200533379c8a
ofekcMoveo <ofekc@moveo.co.il>
2023-05-17 18:11:23 +0300

    firebase auth working add firebase repository

---
+++ Dispatcher/Networking/FirebaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseManager.swift
+//  Dispatcher
+//
+//  Created by Ofek Cohen on 17/05/2023.
+//
+
+import Foundation
---
+++ Dispatcher/Repositories/FirebaseRepository.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseRepository.swift
+//  Dispatcher
+//
+//  Created by Ofek Cohen on 17/05/2023.
+//
+
+import Foundation
